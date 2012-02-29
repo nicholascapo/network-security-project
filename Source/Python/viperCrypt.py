e7561e242ee54095003a54840da5af687eb08210
'Nicholas Capo' <'Nicholas Capo'@localhost>
2012-02-28 20:57:20 -0600

    Initial (Empty) version of viperCrypt.py

---
+++ Source/Python/viperCrypt.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+#########################################
+def parse_S_Boxes():
+	pass
+		
+#########################################
+def parse_Permutations():
+	pass
+
+#########################################
+def encrypt():
+	pass
+
+#########################################
+def decrypt():
+	pass
+
+#########################################
+def initialPermutation():
+	pass
+
+#########################################
+def finalPermutation():
+	pass
+
+#########################################
+def keystream():
+	pass
+	
+#########################################
+def sBox():
+	pass
+
+#########################################
+def linearTransform():
+	pass
+
+#########################################
+def do_round():
+	pass
+
+

c27afbaa4fdb4d28cb5c423d5d86eada0cc0c2b4
Nicholas Capo <Nicholas Capo'@localhost>
2012-02-29 20:48:18 -0600

    Implemented encrypt() and do_round(), code not tested

--- Source/Python/viperCrypt.py
+++ Source/Python/viperCrypt.py
@@ -7,6 +7,7 @@ permutations_file = '../permutations.text'
 sboxes = {}
 permutations = []
 inversePermutations = []
+ROUND_COUNT = 32
 
 #########################################
 def parse_S_Boxes():
@@ -50,7 +51,16 @@ def parse_Permutations():
 	
 #########################################
 def encrypt():
-	pass
+	b = '' # intermediate value
+	b.number = 0
+	for i in xrange(ROUND_COUNT - 1):
+		b = do_round(b)
+		b.number += 1
+	
+	# need last round change instead of doing linear transformation
+
+	assert b.number == 31
+	return b
 
 #########################################
 def decrypt():
@@ -76,10 +86,43 @@ def sBox(data, sBox):
 	return result
 
 #########################################
-def linearTransform():
+def linearTransform(a, b, c, d):
 	pass
+	
 
 #########################################
-def do_round():
-	pass
+def do_round(data, subkey, sbox):
+	result = ''
+	intermidiate = ''
+	# 1. Key Mixing: At each round, a 128-bit subkey Ki is exclusive or’ed
+	# with the current intermediate data Bi
+	
+	assert len(data) == len(subkey)
+	intermediate = data ^ subkey
 
+	# 2. S-Boxes: The 128-bit combination of input and key is considered as
+	# four 32-bit words. The S-box, which is implemented as a sequence of
+	# logical operations (as it would be in hardware) is applied to these
+	# four words, and the result is four output words. The CPU is thus
+	# employed to execute the 32 copies of the S-box simultaneously,
+	# resulting with Si(Bi XOR Ki)
+
+	a, b, c, d = intermediate[::4]
+	assert len(a) == 4
+	assert len(b) == 4
+	assert len(c) == 4
+	assert len(d) == 4
+
+	a, b, c, d = sbox(a, b, c, d)
+	
+	# 3. Linear Transformation: The 32 bits in each of the output words are
+	# linearly mixed
+	
+	intermediate = linearTransform(a, b, c, d)
+	
+	result = a + b + c + d
+	
+	return result
+	
+	
+## EOF ##
